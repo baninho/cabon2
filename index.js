fd58c5bd4408edb3c9115721251aa2b95d1f8c79
baninho <florianimbt@gmail.com>
2021-04-24 18:13:08 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/test', (req, res) => {
+  const count = 5;
+
+  const data = Array(count).fill('test')
+
+  // Return them as json
+  res.json(data);
+
+  console.log('Sent response');
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
+
