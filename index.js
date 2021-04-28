d1fbfa0d49f8fe612fdeedce229859057372821f
baninho <florianimbt@gmail.com>
2021-04-28 12:04:17 -0500

    remove route to /

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/game/:id', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/game/', (req, res) => {
   res.redirect('/game/' + Math.random());
 });
 
