31d10fc2fc7b9f467d53b88847e1cc07b0ee3b45
baninho <florianimbt@gmail.com>
2021-05-05 13:27:32 -0500

    add BoardView class

---
+++ BoardView.js
@@ -0,0 +1,50 @@
+const { CARD_SLOTS, DRAW_IND, DISCARD_IND } = require('./Game');
+
+module.exports = class BoardView {
+  #labels;
+  #wasUpdated;
+
+  constructor(labels) {
+    this.#labels = labels;
+    this.#wasUpdated = true;
+  }
+
+  updateLabel(i, label) {
+    labels[i] = label;
+    this.#wasUpdated = true;
+  }
+
+  updatePlayer(player, playerInd) {
+    player.cards.forEach((c, i) => {
+      this.#labels[i + playerInd*CARD_SLOTS] = c.label;
+    });
+
+    this.#wasUpdated = true;
+  }
+
+  updatePlayers(players) {
+    players.forEach((p, i) => {
+      this.updatePlayer(p, i);
+    });
+
+    this.#wasUpdated = true;
+  }
+
+  updateAll(players, stacks) {
+    this.updatePlayers(players);
+
+    this.#labels[DRAW_IND] = stacks.main[stacks.main.length - 1].label;
+    this.#labels[DISCARD_IND] = stacks.discard[stacks.discard.length - 1].label;
+
+    this.#wasUpdated = true;
+  }
+
+  getLabels() {
+    this.#wasUpdated = false;
+    return this.#labels;
+  }
+
+  wasUpdated() {
+    return this.#wasUpdated;
+  }
+}
